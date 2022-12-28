a7c432108cee8d275be952f2244ca743d33abad0
unknown <bricetatchou68@gmail.com>
2022-12-28 11:41:57 +0100

    Ajout Template Login OK

--- backend/ISEP_FM/settings.py
+++ backend/ISEP_FM/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'account.apps.AccountConfig',
+    'bulletin.apps.BulletinConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -121,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    
+]
